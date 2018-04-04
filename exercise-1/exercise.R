5b4552826d2b52a5edb01c7ed6e0c9c48a4aef2e
Naman Mehra <naman2609@gmail.com>
2018-04-04 16:59:32 -0700

    done

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,22 +2,22 @@
 
 # Create a vector of the number of points the Seahawks scored in the first 4 games
 # of the season (google "Seahawks" for the scores!)
-
+sea <- c(25, 30, 55, 40)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each of the first 4 games of the season
-
+opp <- c(20, 30, 25, 21)
 
 # Combine your two vectors into a dataframe called `games`
-
+games <- data.frame(sea, opp)
 
 # Create a new column "diff" that is the difference in points between the teams
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
-
+games$diff <- games$sea - games$opp
 
 # Create a new column "won" which is TRUE if the Seahawks won the game
-
+games$won <- games$diff > 0
 
 # Create a vector of the opponent names corresponding to the games played
 
